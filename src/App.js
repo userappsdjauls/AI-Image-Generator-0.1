e60c96839cbd3ffa62d7f5bfaf27b696697175fb
userappsdjauls <tikofous@gmail.com>
2025-06-25 04:59:50 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 // --- HELPER FUNCTIONS ---
 const toBase64 = file => new Promise((resolve, reject) => {
